8486d0f626046a317c9bc96ed7c7e2057fe842e3
Stefano Mazzucco <stefano@curso.re>
2016-08-14 15:01:31 +0100

    Fix indentation

--- pulseaudio_dbus.lua
+++ pulseaudio_dbus.lua
@@ -1,30 +1,30 @@
 --[[
-Copyright 2016 Stefano Mazzucco
+  Copyright 2016 Stefano Mazzucco
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
   http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 ]]
 
 --[[--
-Control audio devices using the
-[PulseAudio DBus interface](https://www.freedesktop.org/wiki/Software/PulseAudio/Documentation/Developer/Clients/DBus/).
+  Control audio devices using the
+  [PulseAudio DBus interface](https://www.freedesktop.org/wiki/Software/PulseAudio/Documentation/Developer/Clients/DBus/).
 
-For this to work, you need the line
-`load-module module-dbus-protocol`
-in the `/etc/pulse/default.pa` configuration file.
+  For this to work, you need the line
+  `load-module module-dbus-protocol`
+  in the `/etc/pulse/default.pa` configuration file.
 
-@license Apache License, version 2.0
-@author Stefano Mazzucco <stefano AT curso DOT re>
-@copyright 2016 Stefano Mazzucco
+  @license Apache License, version 2.0
+  @author Stefano Mazzucco <stefano AT curso DOT re>
+  @copyright 2016 Stefano Mazzucco
 ]]
 
 local ldbus = require("ldbus_api")
@@ -127,7 +127,7 @@ local function get_volume(address, sink)
       {sig = ldbus.types.string,
        value = "Volume"}
     }
-}
+  }
 
   local status, data = pcall(ldbus.api.call, opts)
 
